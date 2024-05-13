83fbaa35a59a856fb51a164125c395bfe2df3d19
Jhan24b <jhan24b@gmail.com>
2024-05-13 16:01:50 -0500

    improving academics

--- src/app/components/layout/Academic.js
+++ src/app/components/layout/Academic.js
@@ -66,22 +66,6 @@ export default function Academic() {
         <h2 className="">Formación Académica</h2>
         <EducationSection title="Grados" items={grades} />
         <EducationSection title="Especializaciones" items={specialization} />
-        {/* <h3 className="text-cerulean-blue-800 font-bold text-[30px]">Grados</h3>
-        <div className="p-12">
-          <div className="flex justify-center items-center flex-col md:flex-row gap-12 md:gap-8 flex-wrap">
-            {grades.map((data, idx) => {
-              return <CardGrade key={idx} data={data} />;
-            })}
-          </div>
-        </div>
-        <h3 className="text-cerulean-blue-800 font-bold text-[30px]">Especializaciones</h3>
-        <div className="p-12">
-          <div className="flex justify-center items-center flex-col md:flex-row gap-12 md:gap-8 flex-wrap">
-            {specialization.map((data, idx) => {
-              return <CardGrade key={idx} data={data} />;
-            })}
-          </div>
-        </div> */}
       </div>
     </section>
   );
